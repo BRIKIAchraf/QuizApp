cbeee867748848ffacfdeabe685299d47faa0e01
AchrafbrekiDSI2022 <brikiachraf@DJERBA.r-iset.tn>
2023-09-17 17:45:16 +0200

    complete the result component and configure the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ResultComponent } from './result/result.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { ResultComponent } from './result/result.component';
     ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
