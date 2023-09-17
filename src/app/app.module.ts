6a26bfc21520c0079a31a3fe698c08acf2067210
AchrafbrekiDSI2022 <brikiachraf@DJERBA.r-iset.tn>
2023-09-17 13:06:44 +0200

    add service and component and installe bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule
